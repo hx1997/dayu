451a338b7a29ad44bf5fdedd2cd9907496d10f52
hx1997 <hx970923@gmail.com>
2024-09-07 15:56:57 +0800

    fix: don't remove the third argument in MethodCallPrettify because some calls are function calls and their third argument is not "this"

--- decompile/passes/method_call_prettify.py
+++ decompile/passes/method_call_prettify.py
@@ -6,7 +6,7 @@ from decompile.method_pass import MethodPass
 
 class MethodCallPrettify(MethodPass):
     """
-    turn "this.v0 = func(FunctionObject, NewTarget, this, ...)" into "this.v0 = func(...)"
+    turn "this.v0 = func(FunctionObject, NewTarget, this, ...)" into "this.v0 = func(this, ...)"
     """
     def run_on_method(self, method: IRMethod):
         for block in method.blocks:
@@ -15,7 +15,7 @@ class MethodCallPrettify(MethodPass):
     def run_on_block(self, block: IRBlock):
         for insn in block.insns:
             if insn.type == NAddressCodeType.CALL:
-                if len(insn.args) < 5:  # v0 = func(FunctionObject, NewTarget, this, ...), at least five arguments
+                if len(insn.args) < 4:  # v0 = func(FunctionObject, NewTarget, ...), at least five arguments
                     continue
                 if insn.args[2].type == 'FunctionObject' and insn.args[3].type == 'NewTarget':
-                    insn.args[2:] = insn.args[5:]
+                    insn.args[2:] = insn.args[4:]
