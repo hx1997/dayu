4d59e7bf48e99e943e3ea8d5480140f9218796df
hx1997 <hx970923@gmail.com>
2026-05-14 01:59:36 +0800

    refactor: use named fields in peephole_opt.py and method_call_prettify.py (phase 5)

--- decompile/passes/method_call_prettify.py
+++ decompile/passes/method_call_prettify.py
@@ -15,7 +15,7 @@ class MethodCallPrettify(MethodPass):
     def run_on_block(self, block: IRBlock):
         for insn in block.insns:
             if insn.type == NAddressCodeType.CALL:
-                if len(insn.args) < 4:  # v0 = func(FunctionObject, NewTarget, ...), at least four arguments
+                if len(insn.call_args) < 2:  # func(FunctionObject, NewTarget, ...), at least two call args
                     continue
-                if insn.args[2].type == 'FunctionObject' and insn.args[3].type == 'NewTarget':
-                    insn.args[2:] = insn.args[4:]
+                if insn.call_args[0].type == 'FunctionObject' and insn.call_args[1].type == 'NewTarget':
+                    insn.call_args = insn.call_args[2:]
--- decompile/passes/peephole_opt.py
+++ decompile/passes/peephole_opt.py
@@ -31,7 +31,7 @@ class PeepholeOptimization(MethodPass):
                 continue
             # case 0: eliminate "a = a", which could result from CopyPropagation replacing copies
             # take extra care that the rhs doesn't have an operator
-            if insn.type == NAddressCodeType.ASSIGN and insn.args[0] == insn.args[1] and insn.op == '':
+            if insn.type == NAddressCodeType.ASSIGN and insn.dst == insn.src and insn.op == '':
                 insn.erase_from_parent()
                 continue
             if self.constrained:
@@ -54,14 +54,14 @@ class PeepholeOptimization(MethodPass):
                 # if we proceed to the following cases we'll be looking at a non-existent instruction
                 continue
             # case 1: eliminate "a = b; b = a" by removing "b = a"
-            elif (insn.args[1] == block_insns_copy[idx - 1].args[0] and insn.args[0] == block_insns_copy[idx - 1].args[1]
+            elif (insn.src == block_insns_copy[idx - 1].dst and insn.dst == block_insns_copy[idx - 1].src
                   and insn.op == '' and block_insns_copy[idx - 1].op == ''):
                 insn.erase_from_parent()
             # case 2: collapse "a = b; a = a['xxx']" into "a = b['xxx']"
             # take extra care that the two instructions don't both have an operator on the rhs
-            elif (insn.args[1].ref_obj == block_insns_copy[idx - 1].args[0] and insn.args[0] == insn.args[1].ref_obj
+            elif (insn.src.ref_obj == block_insns_copy[idx - 1].dst and insn.dst == insn.src.ref_obj
                   and not (insn.op and block_insns_copy[idx - 1].op)):
-                insn.args[1].ref_obj = block_insns_copy[idx - 1].args[1]
+                insn.src.ref_obj = block_insns_copy[idx - 1].src
                 # if the first instruction has a rhs operator, copy it to the second one before erasing
                 if insn.op == '':
                     insn.op = block_insns_copy[idx - 1].op
@@ -71,37 +71,37 @@ class PeepholeOptimization(MethodPass):
                 # the second instruction, so analyze liveness here
                 if idx + 1 < len(block_insns_copy):
                     live_vars = DeadCodeElimination(self.out_l).analyze_block(block, False, block_insns_copy[idx + 1])
-                    if block_insns_copy[idx - 1].args[0] in live_vars or block_insns_copy[idx - 1].args[0].ref_obj in live_vars:
+                    if block_insns_copy[idx - 1].dst in live_vars or block_insns_copy[idx - 1].dst.ref_obj in live_vars:
                         continue
                 # case 3: collapse "a = b; c = a" into "c = b"
                 # take extra care that the two instructions don't both have an operator on the rhs
-                if insn.args[1] == block_insns_copy[idx - 1].args[0] and not (insn.op and block_insns_copy[idx - 1].op):
+                if insn.src == block_insns_copy[idx - 1].dst and not (insn.op and block_insns_copy[idx - 1].op):
                     # in constrained mode,
                     # make sure b and c don't both have a ref_obj, since we don't want "c['xxx'] = b['yyy']" just yet
                     # as it will break the three-argument form of ASSIGN NACs and complicate analysis
-                    if block_insns_copy[idx - 1].args[1].ref_obj is None or insn.args[0].ref_obj is None or not self.constrained:
-                        insn.args[1] = block_insns_copy[idx - 1].args[1]
+                    if block_insns_copy[idx - 1].src.ref_obj is None or insn.dst.ref_obj is None or not self.constrained:
+                        insn.src = block_insns_copy[idx - 1].src
                         # if the first instruction has a rhs operator, copy it to the second one before erasing
                         if insn.op == '':
                             insn.op = block_insns_copy[idx - 1].op
                         block_insns_copy[idx - 1].erase_from_parent()
                 # case 4: collapse "a = b; c = a['xxx']" into "c = b['xxx']"
-                elif insn.args[1].ref_obj and insn.args[1].ref_obj == block_insns_copy[idx - 1].args[0] and not (insn.op and block_insns_copy[idx - 1].op):
+                elif insn.src.ref_obj and insn.src.ref_obj == block_insns_copy[idx - 1].dst and not (insn.op and block_insns_copy[idx - 1].op):
                     # in constrained mode,
                     # make sure b doesn't have a ref_obj itself, since we don't want chained ref_obj just yet
                     # as it will break the three-argument form of ASSIGN NACs and complicate analysis
-                    if block_insns_copy[idx - 1].args[1].ref_obj is None or not self.constrained:
-                        insn.args[1].ref_obj = block_insns_copy[idx - 1].args[1]
+                    if block_insns_copy[idx - 1].src.ref_obj is None or not self.constrained:
+                        insn.src.ref_obj = block_insns_copy[idx - 1].src
                         if insn.op == '':
                             insn.op = block_insns_copy[idx - 1].op
                         block_insns_copy[idx - 1].erase_from_parent()
                 # case 5: collapse "a = b; a['xxx'] = c" into "b['xxx'] = c"
-                elif insn.args[0].ref_obj and insn.args[0].ref_obj == block_insns_copy[idx - 1].args[0] and block_insns_copy[idx - 1].op == '':
+                elif insn.dst.ref_obj and insn.dst.ref_obj == block_insns_copy[idx - 1].dst and block_insns_copy[idx - 1].op == '':
                     # in constrained mode,
                     # make sure b doesn't have a ref_obj itself, since we don't want chained ref_obj just yet
                     # as it will break the three-argument form of ASSIGN NACs and complicate analysis
-                    if block_insns_copy[idx - 1].args[1].ref_obj is None or not self.constrained:
-                        insn.args[0].ref_obj = block_insns_copy[idx - 1].args[1]
+                    if block_insns_copy[idx - 1].src.ref_obj is None or not self.constrained:
+                        insn.dst.ref_obj = block_insns_copy[idx - 1].src
                         block_insns_copy[idx - 1].erase_from_parent()
 
     def eliminate_stld_lexvar_pattern(self, idx, insn, block_insns_copy):
@@ -124,21 +124,21 @@ class PeepholeOptimization(MethodPass):
             return False
         if block_insns_copy[idx - 1].type != NAddressCodeType.CALL or block_insns_copy[idx - 3].type != NAddressCodeType.CALL:
             return False
-        if insn.args[0].type != 'acc' or insn.args[1].type != 'field':
+        if insn.dst.type != 'acc' or insn.src.type != 'field':
             return False
-        if block_insns_copy[idx - 2].args[0].type != 'field' or block_insns_copy[idx - 2].args[1].type != 'acc':
+        if block_insns_copy[idx - 2].dst.type != 'field' or block_insns_copy[idx - 2].src.type != 'acc':
             return False
-        if not insn.args[1].ref_obj or insn.args[1].ref_obj.type != 'lexenv':
+        if not insn.src.ref_obj or insn.src.ref_obj.type != 'lexenv':
             return False
-        if not block_insns_copy[idx - 2].args[0].ref_obj or block_insns_copy[idx - 2].args[0].ref_obj.type != 'lexenv':
+        if not block_insns_copy[idx - 2].dst.ref_obj or block_insns_copy[idx - 2].dst.ref_obj.type != 'lexenv':
             return False
-        if insn.args[1].ref_obj.value != block_insns_copy[idx - 2].args[0].ref_obj.value:
+        if insn.src.ref_obj.value != block_insns_copy[idx - 2].dst.ref_obj.value:
             return False
-        if block_insns_copy[idx - 1].args[0].type != 'lexenv' or block_insns_copy[idx - 1].args[1].value != '__get_lexenv__':
+        if block_insns_copy[idx - 1].dst.type != 'lexenv' or block_insns_copy[idx - 1].func.value != '__get_lexenv__':
             return False
-        if block_insns_copy[idx - 3].args[0].type != 'lexenv' or block_insns_copy[idx - 3].args[1].value != '__get_lexenv__':
+        if block_insns_copy[idx - 3].dst.type != 'lexenv' or block_insns_copy[idx - 3].func.value != '__get_lexenv__':
             return False
-        if block_insns_copy[idx - 1].args[3].value != block_insns_copy[idx - 3].args[3].value:
+        if block_insns_copy[idx - 1].call_args[1].value != block_insns_copy[idx - 3].call_args[1].value:
             return False
         return True
 
@@ -157,12 +157,12 @@ class PeepholeOptimization(MethodPass):
             if (insn.type not in [NAddressCodeType.COND_JUMP, NAddressCodeType.COND_THROW]
                     or block_insns_copy[idx - 1].type != NAddressCodeType.ASSIGN):
                 continue
-            if insn.args[0] == block_insns_copy[idx - 1].args[0]:
-                if insn.op in ['==', '!='] and insn.args[1].type == 'zero' and block_insns_copy[idx - 1].is_relational_operation():
+            if insn.cond1 == block_insns_copy[idx - 1].dst:
+                if insn.op in ['==', '!='] and insn.cond2.type == 'zero' and block_insns_copy[idx - 1].is_relational_operation():
                     if insn.op == '==' and not block_insns_copy[idx - 1].invert_relational_operation():
                         continue
-                    insn.args[0] = block_insns_copy[idx - 1].args[1]
+                    insn.cond1 = block_insns_copy[idx - 1].src
                     insn.op = block_insns_copy[idx - 1].op
-                    insn.args[1] = block_insns_copy[idx - 1].args[2]
+                    insn.cond2 = block_insns_copy[idx - 1].src2
                     block_insns_copy[idx - 1].erase_from_parent()
         pass
